3862612817ba2927366d71e3eb3d2217f6c54165
Alden Summerville <ads3pu@virginia.edu>
2021-04-07 17:18:25 -0400

    added "steps"

---
+++ main.py
@@ -0,0 +1,21 @@
+#steps:
+
+##SCRIPTING
+
+#figure out what data source i'll use. possibly a url source (multiple)
+
+#write a script to ingest said data (include error handling)
+
+#write another script to convert the file type of said data (include error handling)
+
+#script to clean/transform the data. add/remove columns, etc. (include error handling)
+
+#script that connects to a sql db and pushes the file to said db (include error handling)
+
+#make sure to include a summary of what happend and where the file ended up
+
+#write a "main" script that asks for the parameter and then just executes each of the above scripts
+
+##DOCKER
+
+#create a dockerfile in this folder that maps the folder and has a CMD to execute the main script

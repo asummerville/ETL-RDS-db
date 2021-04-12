b14ca679be127937a4b4b9ec6cfdb0c19060ede9
asummerville <ads3pu@virginia.edu>
2021-04-11 23:24:18 -0400

    additions

--- ingest.py
+++ ingest.py
@@ -10,7 +10,6 @@ api.authenticate()
 
 file = str(os.getenv('file'))
 
-#file = 'data_by_artist.csv'
 api.dataset_download_file('yamaerenay/spotify-dataset-19212020-160k-tracks', file_name=file, path="./")
 
 with zipfile.ZipFile(f'./{file}.zip', 'r') as zipref:
